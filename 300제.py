7530cc751697ce2aa2c68db34837cc59eb1fc54b
dhfhrn <145188889+dhfhrn@users.noreply.github.com>
2023-09-16 17:31:07 +0900

    Add files via upload

---
+++ 300제.py
@@ -0,0 +1,50 @@
+# print('Hello world')
+#
+# print ('신씨가 소리질렀다. "도둑이야".')
+#
+# print("안녕하세요.\n만나서\t\t반갑습니다.")
+#
+# print ('naver','kakao','samsung', sep=";")
+#
+# print("first",end=' ');print("second")
+
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액,type(시가총액))
+print(현재가,type(현재가))
+print(PER,type(PER))
+
+s = "hello"
+t = "python"
+print(s+"!",t)
+
+print(2+2*3)
+
+a = 132
+print(type(a))
+
+num_str = "720"
+num_int = int(num_str)
+print(num_int+1,type(num_int))
+
+num = 100
+result = str(num)
+print(result,type(result))
+
+a = 15.79
+a = float(a)
+print(a, type(a))
+
+year = "2020"
+print(int(year)-3)
+print(int(year)-2)
+print(int(year)-1)
+
+월 = 48584
+총금액 = 월 * 36
+print(총금액)
---
+++ 자료형.py
@@ -0,0 +1,65 @@
+# # 정수 int 실수 float
+# a = 3
+# b = 9
+# print (a + b)
+# print (a - b)
+# print (a * b)
+# print (a / b)
+# print (a // b)
+# print (a % b)
+# print (a ** b)
+
+#문자열 str
+#문자열도 연산 가능
+#문자열 길이
+# print(len (a))
+# a = 'Hello World'
+# print (a,count('o')) #해당 문자의 개수 변환
+# print (a.index('l')) #해당 문자의 인덱스 번호 변환
+# print (a.find('x')) #해당 문자의 인덱스 번호 반환, 없는 문자는 -1로 반환
+# print (' '.join(a)) #해당 문자의 인덱스 사이사이에 추가한 값을 반환
+# print (a.relace('l','.')) #앞의 값을 뒤의 값으로 바꿔서 반환
+# print (a.split('o')) #해당 문자를 기준으로 쪼개서 반환
+# print (a.upper()) #문자열 모두 대문자로 바꿔서 반환
+
+#
+# l = [1,2,3,4,]
+# print (l[1:3])
+# print (l[2]) #인덱싱
+# print (l[1:3])#슬라이싱
+# l = [1,[2,3,[4,5]]]
+# print (l[1][2][0]) #n중 리스트에서의 인덱싱
+#
+# a = [4,1,2,3]
+# b = [5,6,7]
+#
+# a[3] = 7
+# print (a)
+
+
+
+a = [4,1,2,3]
+# b = [5,8]
+# a.append(5) #맨 뒤에 해당값을 추가
+# print(a)
+# a.reverse( )#리스트 순서 반전
+# print (a)
+# a. insert (2, 6) #앞의 인덱스 번호 자리에 뒤릐 데이터 삽입
+# print (a)
+# a.pop() #빈 괄호일 땐 맨 뒤의 값, 숫자가 있을 땐 해당 인덱스 번호 값을 가져옴
+# print (a)
+# a.sort()#리스트의 값을 오름차순으로 정령
+# print(a)
+# a.sort(reverse = Ture) #리스트 값을 내림차순으로 정렬
+# print(a)
+# print(a.count(3))
+# print(a.index(3))
+# a.remove(3)#해당 값 삭제
+# print(a)
+# a.extend([0,7]) #괄호속 리스트에 값들을 넣어 리스트를 확장
+# print(a)
+# a.append([0,7])
+# print(a)
+
+
+
