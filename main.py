564afc377362360befe714ef2074e377951f620c
dhfhrn <145188889+dhfhrn@users.noreply.github.com>
2023-10-14 13:49:16 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,113 @@
-# def calculate(a,op,b):
-#     if op == '+':
-#         return a + b
-#     elif op == '-':
-#         return a - b
-#     elif op == '/':
-#         if b == 0:
-#             return 0
-#         return a / b
-#     elif op == '*':
-#         return a * b
+# def 함수(문자열) :
+#     print(문자열)
 #
-# a = 3
-# b = 4
-# op1 = '+'
-# op2 = '-'
-# op3 = '/'
-# op4 = '*'
+# 함수("안녕")
+# 함수("Hi")
+# 안녕
+# Hi
+
+# def 함수(a, b) :
+#     print(a + b)
 #
-# print(calculate(a,op1,b))
-# print(calculate(a,op2,b))
-# print(calculate(a,op3,b))
-# print(calculate(a,op3,0))
-# print(calculate(a,op4,b))
+# 함수(3, 4)
+# 함수(7, 8)
+# 7
+# 15
+
+# def 함수(a, b) :
+#     print(a + b)
 #
-# def factorial(x):
-#     a = 1
-#     for b in range(1,x+1):
-#         a *= b
-#     return a
-#
-# def solution(balls, share):
-#     answer = factorial(balls) / (factorial(balls - share) * factorial(share))
-#     return answer
+# 함수("안녕", 3)
+# 문자열과 숫자를 동시에 사용할 수 없기 때문이다
 
+# def  print_with_smil (string):
+#     print(string + ":D")
 
+# print_with_smile("안녕하세요")
 
+# def print_upper_price (price):
+#     print(price * 1.3)
 
+# def print_sum (a,b):
+#     print(a + b)
 
+# def print_arithmetic_operation(a,b):
+#     print(a, "+", b, '=', a + b)
+#     print(a, "-", b, '=', a - b)
+#     print(a, "*", b, '=', a * b)
+#     print(a, "/", b, '=', a / b)
 
+# def print_max (a, b, c):
+#     max_val = 0
+#     if a > max_val:
+#         max_val = a
+#     if b > max_val:
+#         max_val = b
+#     if c > max_val:
+#         max_val = c
+#     print(max_val)
 
+# def  make_url (string):
+#     nums = 'www.' + string + ".com"
+#     return nums
+
+# def make_list (string):
+#     return list (string)
+
+# def pickup_even (dog):
+#     result = 0
+#     for dog in dog:
+#         if dog % 2 == 0:
+#             result.append(dog)
+#             return result
+
+# def  convert_in (string):
+#     return int(string.replace(',',""))
+
+# a = int(input())
+# answer = 'int'
+# for i in range(a//4):
+#     answer = 'long' + answer
+#     print (answer)
+
+# for i in range(int(input())//4):
+#     print('long',end=" ")
+
+# print('int')
+
+# t = int(input())
+# for i in range(1, t+1):
+#     a,b = map(int,input().split())
+#     print(f'Case #{i}: {a+b}')
+
+# t = int(input())
+# for x in range(1, t+1):
+#     a, b = map(int, input().split())
+#     print(f'Case #{x}: {a} + {b} = {a+b}')
+
+# n = int(input())
+# for i in range(1, n+1):
+#     print("*" * i)
+
+# n = int(input())
+# for i in range(1, n+1):
+# 	print(' '*(n-i) + '*'*i)
+
+# while True:
+#     a, b = map(int, input().split())
+#     if a == 0 and b == 0:
+#         break;
+#     else:
+#         print(a+b)
+
+# while True:
+#     try:
+#         A, B = map(int, input().split())
+#         print(A+B)
+#     except:
+#         break
+
+# s = input()
+# i = int(input())
+# print(s[i-1])
 
