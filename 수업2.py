6bfd7930b0be9bb3699ecd1b76269fc5651b56fb
dhfhrn <145188889+dhfhrn@users.noreply.github.com>
2023-10-15 21:56:18 +0900

    Add files via upload

---
+++ 수업2.py
@@ -0,0 +1,19 @@
+def solution(n):
+    answer = []
+    for i in range(n):
+        temp = []
+        for j in range(n):
+            temp.append(0)
+        answer.append(temp)
+    x = 0
+    y = 0
+
+    for i in range(n*n):
+        answer [x][y] = n+1
+
+
+
+
+
+
+    return answer
