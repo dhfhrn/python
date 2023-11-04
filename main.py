9c4bea6d070bae5b9bcf61e35ff8c28af9c363f9
dhfhrn <145188889+dhfhrn@users.noreply.github.com>
2023-11-04 13:41:00 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,113 +1,27 @@
-# def 함수(문자열) :
-#     print(문자열)
-#
-# 함수("안녕")
-# 함수("Hi")
-# 안녕
-# Hi
-
-# def 함수(a, b) :
-#     print(a + b)
-#
-# 함수(3, 4)
-# 함수(7, 8)
-# 7
-# 15
-
-# def 함수(a, b) :
-#     print(a + b)
-#
-# 함수("안녕", 3)
-# 문자열과 숫자를 동시에 사용할 수 없기 때문이다
-
-# def  print_with_smil (string):
-#     print(string + ":D")
-
-# print_with_smile("안녕하세요")
-
-# def print_upper_price (price):
-#     print(price * 1.3)
-
-# def print_sum (a,b):
-#     print(a + b)
-
-# def print_arithmetic_operation(a,b):
-#     print(a, "+", b, '=', a + b)
-#     print(a, "-", b, '=', a - b)
-#     print(a, "*", b, '=', a * b)
-#     print(a, "/", b, '=', a / b)
-
-# def print_max (a, b, c):
-#     max_val = 0
-#     if a > max_val:
-#         max_val = a
-#     if b > max_val:
-#         max_val = b
-#     if c > max_val:
-#         max_val = c
-#     print(max_val)
-
-# def  make_url (string):
-#     nums = 'www.' + string + ".com"
-#     return nums
-
-# def make_list (string):
-#     return list (string)
-
-# def pickup_even (dog):
-#     result = 0
-#     for dog in dog:
-#         if dog % 2 == 0:
-#             result.append(dog)
-#             return result
-
-# def  convert_in (string):
-#     return int(string.replace(',',""))
-
-# a = int(input())
-# answer = 'int'
-# for i in range(a//4):
-#     answer = 'long' + answer
-#     print (answer)
-
-# for i in range(int(input())//4):
-#     print('long',end=" ")
-
-# print('int')
+# n, t, p, c = map(int, input().split())
+# print((n - 1)//t * p * c)
 
 # t = int(input())
-# for i in range(1, t+1):
-#     a,b = map(int,input().split())
-#     print(f'Case #{i}: {a+b}')
-
-# t = int(input())
-# for x in range(1, t+1):
-#     a, b = map(int, input().split())
-#     print(f'Case #{x}: {a} + {b} = {a+b}')
-
-# n = int(input())
-# for i in range(1, n+1):
-#     print("*" * i)
-
-# n = int(input())
-# for i in range(1, n+1):
-# 	print(' '*(n-i) + '*'*i)
-
-# while True:
-#     a, b = map(int, input().split())
-#     if a == 0 and b == 0:
-#         break;
+# for i in range(t):
+#     m, n = map(int, input().split())
+#
+#     if m < 12 or n < 4:
+#         print(-1)
 #     else:
-#         print(a+b)
+#         print(11 * n + 4)
 
-# while True:
-#     try:
-#         A, B = map(int, input().split())
-#         print(A+B)
-#     except:
-#         break
+while True:
+    ans = 0
+    N = input()
+    if N == '0':
+        break
+    ans += len(N)+1
+    for i in N:
+        if i == '0':
+            ans += 4
+        elif i == '1':
+            ans += 2
+        else:
+            ans += 3
 
-# s = input()
-# i = int(input())
-# print(s[i-1])
-
+    print(ans)
